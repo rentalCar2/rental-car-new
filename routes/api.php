c97dd36b524ce18657952a0ce1838c103563d296
Jo Li <jackliahu@gmail.com>
2024-06-12 10:39:48 +0800

    init: 修改readme 以及配置信息

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+// v1 版本
+Route::prefix('/v1')->group(function () {
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
 });
+
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
